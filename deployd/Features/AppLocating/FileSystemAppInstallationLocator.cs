ef4f989e92552ec052f56103531371ade737361a
David Whitney <david@davidwhitney.co.uk>
2013-01-26 01:30:26 +0000

    disk discovery.

--- deployd/Features/AppLocating/FileSystemAppInstallationLocator.cs
+++ deployd/Features/AppLocating/FileSystemAppInstallationLocator.cs
@@ -1,15 +1,62 @@
-﻿namespace deployd.Features.AppLocating
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Abstractions;
+using System.Linq;
+using deployd.Features.ClientConfiguration;
+using deployd.Infrastructure;
+using log4net;
+
+namespace deployd.Features.AppLocating
 {
-    public class FileSystemAppInstallationLocator<TPackageType> : IAppInstallationLocator<TPackageType>
+    public class FileSystemAppInstallationLocator : IAppInstallationLocator<PackagePointer>
     {
-        public PackageLocation<TPackageType> CanFindPackage(string appName)
+        private readonly IFileSystem _fs;
+        private readonly DeploydConfiguration _configuration;
+        private readonly ILog _log;
+
+        public FileSystemAppInstallationLocator(IFileSystem fs, DeploydConfiguration configuration, ILog log)
         {
+            _fs = fs;
+            _configuration = configuration;
+            _log = log;
+        }
+
+        public PackageLocation<PackagePointer> CanFindPackage(string appName)
+        {
+            try
+            {
+                var searchPath = _configuration.PackageSource;
+                var matching = _fs.Directory.GetFiles(searchPath, appName + ".*.zip", SearchOption.AllDirectories).ToList();
+                matching.Reverse();
+
+                var latestPackage = matching.FirstOrDefault();
+
+                if (latestPackage != null)
+                {
+                    return new PackageLocation<PackagePointer>
+                        {
+                            PackageDetails = new PackagePointer {PathAndFileName = latestPackage.ToAbsolutePath()}
+                        };
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Error("Could not get package from file system", ex);
+            }
+
             return null;
         }
 
         public PackageLocation<object> CanFindPackageAsObject(string appName)
         {
-            return null;
+            var inner = CanFindPackage(appName);
+            return inner == null ? null : new PackageLocation<object> { PackageDetails = inner.PackageDetails };
         }
+    }
+
+    public class PackagePointer
+    {
+        public string PathAndFileName { get; set; }
     }
 }

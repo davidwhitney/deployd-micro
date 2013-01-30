d21c3af0613e84d50d313a70407ba114faedb86d
David Whitney <david@davidwhitney.co.uk>
2013-01-30 22:51:32 +0000

    Added deployd "watchmen" sibling service.

---
+++ deployd.watchman/Modules/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Nancy;
+
+namespace deployd.watchman.Modules
+{
+    public class HomeController : NancyModule
+    {
+        public HomeController()
+        {
+            Get["/"] = x =>
+            {
+                
+                return View["index.cshtml"];
+            };
+        }
+    }
+}

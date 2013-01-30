09aaaae7e51d7806cdf45c91081ae8ca3aac450f
David Whitney <david@davidwhitney.co.uk>
2013-01-30 22:52:56 +0000

    Added empty deployd package and deployd remote.

---
+++ deployd-remote/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace deployd_remote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

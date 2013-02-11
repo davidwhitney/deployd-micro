eb64aaa2d0633261532e11f35427597f31b3894e
David Whitney <david@davidwhitney.co.uk>
2013-02-11 23:17:44 +0000

    Test for purging backups.

---
+++ deployd.tests/Features/PurgeOldBackups/PurgeOldBackupsCommandTests.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using NUnit.Framework;
+using deployd.Features;
+using deployd.Features.PurgeOldBackups;
+
+namespace deployd.tests.Features.PurgeOldBackups
+{
+    [TestFixture]
+    public class PurgeOldBackupsCommandTests
+    {
+        [Test]
+        public void Execute_InvokesApplicationMethod()
+        {
+            var app = new Mock<IApplication>();
+            var cmd = new PurgeOldBackupsCommand(app.Object);
+
+            cmd.Execute();
+
+            app.Verify(x=>x.PruneBackups(), Times.Once());
+        }
+    }
+}

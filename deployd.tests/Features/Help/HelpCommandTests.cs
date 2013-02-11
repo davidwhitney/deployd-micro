5d3566ce9d6cc3bcd8d918266cb0c21dbcf6532d
David Whitney <david@davidwhitney.co.uk>
2013-02-11 23:22:57 +0000

    help tests not done yet

---
+++ deployd.tests/Features/Help/HelpCommandTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using deployd.Features.FeatureSelection;
+using deployd.Features.Help;
+
+namespace deployd.tests.Features.Help
+{
+    [TestFixture]
+    public class HelpCommandTests
+    {
+        [Test]
+        public void Unfinished()
+        {
+            var config = new InstanceConfiguration();
+            var cmd = new HelpCommand(config);
+
+        }
+    }
+}

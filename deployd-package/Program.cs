6f00072b0cf7dfe943f070ca2aa2f923dbf0b512
David Whitney <david@davidwhitney.co.uk>
2013-03-10 19:58:53 +0000

    sketching out package auto-gen.

--- deployd-package/Program.cs
+++ deployd-package/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.IO.Abstractions;
 using NuGet;
 
@@ -6,7 +7,7 @@ namespace deployd_package
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             if (args.Length < 1)
             {
@@ -15,12 +16,26 @@ namespace deployd_package
 
             var directoryInspector = new PackageFileLocator(new FileSystem(), args[0]);
 
-            var package = new PackageBuilder();
+            // need to fill in order discover all these bits
+            var package = new PackageBuilder
+                {
+                    Id = "temporary-id",
+                    Version = new SemanticVersion(1, 0, 0, 0),
+                    Description = "desc"
+                };
+            package.Authors.Add("Author");
+
             foreach (var file in directoryInspector.IncludedFiles())
             {
                 package.Files.Add(file);
             }
 
+            var packageOutputFilename = string.Format("{0}-{1}.nupkg", package.Id, package.Version);
+            using (var fs = new FileStream(packageOutputFilename, FileMode.Create))
+            {
+                package.Save(fs);
+                fs.Close();
+            }
         }
     }
 }

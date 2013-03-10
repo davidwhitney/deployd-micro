565f98f91da7f177d8e3cdcd3c600852138baef5
David Whitney <david@davidwhitney.co.uk>
2013-03-10 23:55:23 +0000

    Autopackager rule to infer info from a single matched executable.

--- deployd-package/Features/MetadataDiscovery/InferPackageDetailsFromSingleFoundExecutable.cs
+++ deployd-package/Features/MetadataDiscovery/InferPackageDetailsFromSingleFoundExecutable.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using NuGet;
 
 namespace deployd_package.Features.MetadataDiscovery
 {
@@ -13,14 +15,25 @@ namespace deployd_package.Features.MetadataDiscovery
         
         public void DiscoverMetadataProperties(PackageMetadata discoveredMetadata, string discoveryRoot)
         {
-            var matchingFiles = _fs.Directory.GetFiles(discoveryRoot, "*.exe", System.IO.SearchOption.AllDirectories);
+            var matchingFiles = _fs.Directory.GetFiles(discoveryRoot, "*.exe", System.IO.SearchOption.AllDirectories).ToList();
+            matchingFiles.RemoveAll(x => x.Contains(".vshost"));
 
-            if (matchingFiles.Length != 1)
+            if (matchingFiles.Count != 1)
             {
                 return;
             }
 
             var vi = FileVersionInfo.GetVersionInfo(matchingFiles[0]);
+
+            discoveredMetadata.Id = vi.ProductName;
+            discoveredMetadata.Version = new SemanticVersion(vi.FileMajorPart, vi.FileMinorPart, vi.FileBuildPart, vi.FilePrivatePart);
+            discoveredMetadata.Description = vi.FileDescription;
+
+            if (!string.IsNullOrWhiteSpace(vi.CompanyName))
+            {
+                discoveredMetadata.Authors.Clear();
+                discoveredMetadata.Authors.Add(vi.CompanyName);
+            }
         }
     }
 }

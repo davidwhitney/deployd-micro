ac17f024cc9be21290b3a6ed95afb2080fd715cf
Andrew Myhre <andrew.myhre@gmail.com>
2013-06-03 11:43:05 -0400

    create install layout script also outputs package app

--- create-deployd-install-layout.bat
+++ create-deployd-install-layout.bat
@@ -2,3 +2,4 @@ C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=
 mkdir installed-layout
 xcopy deployd\bin\release\*.* installed-layout /Y
 xcopy deployd.watchman\bin\release\*.* installed-layout /Y
+xcopy deployd-package\bin\release\*.* installed-layout /Y

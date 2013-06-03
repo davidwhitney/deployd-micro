8a68ae9e0f5f0f161c0522cf71cb565609061215
Andrew Myhre <andrew.myhre@gmail.com>
2013-06-03 15:13:17 -0400

    infer metadata from exe was failing when an app contained another exe

--- deployd-package/Features/MetadataDiscovery/InferPackageDetailsFromSingleFoundExecutable.cs
+++ deployd-package/Features/MetadataDiscovery/InferPackageDetailsFromSingleFoundExecutable.cs
@@ -18,7 +18,16 @@ namespace deployd_package.Features.MetadataDiscovery
         {
             var exesFound = _fs.Directory.GetFiles(discoveryRoot, "*.exe", System.IO.SearchOption.AllDirectories).ToList();
             exesFound.RemoveAll(x => x.Contains(".vshost"));
-            
+
+            var expectedApplicationName = _fs.DirectoryInfo.FromDirectoryName(discoveryRoot).Name;
+            var exesFoundWithExpectedName = exesFound.Where(f => f.EndsWith(expectedApplicationName+".exe")).ToArray();
+
+            if (exesFoundWithExpectedName.Length == 1)
+            {
+                _fromAssemblyMapper.MapAssemblyInfoToPackage(exesFoundWithExpectedName[0], discoveredMetadata);
+                return;
+            }
+
             if (exesFound.Count == 1)
             {
                 _fromAssemblyMapper.MapAssemblyInfoToPackage(exesFound[0], discoveredMetadata);
@@ -31,7 +40,11 @@ namespace deployd_package.Features.MetadataDiscovery
             }
 
             var dllsFound = _fs.Directory.GetFiles(discoveryRoot, "*.dll", System.IO.SearchOption.AllDirectories).ToList();
-
+            var dllsFoundWithExpectedName = dllsFound.Where(f => f.EndsWith(expectedApplicationName + ".dll")).ToArray();
+            if (dllsFoundWithExpectedName.Length == 1)
+            {
+                _fromAssemblyMapper.MapAssemblyInfoToPackage(dllsFoundWithExpectedName[0], discoveredMetadata);
+            }
             if (dllsFound.Count == 1)
             {
                 _fromAssemblyMapper.MapAssemblyInfoToPackage(dllsFound[0], discoveredMetadata);

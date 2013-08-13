756b402f70f28d8d2d96eae95cc073921ac0c458
David Whitney <david@davidwhitney.co.uk>
2013-08-13 13:41:58 +0100

    removed space.

--- deployd/Properties/AssemblyInfo.cs
+++ deployd/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("David Whitney")]
 [assembly: AssemblyProduct("deployd")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCopyright("Copyright © 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]

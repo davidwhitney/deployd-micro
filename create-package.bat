8321848013cce460d91cd088a809547e71c1daed
David Whitney <david@davidwhitney.co.uk>
2013-08-13 10:30:06 +0100

    Added build for packager.

--- create-package.bat
+++ create-package.bat
@@ -3,3 +3,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 .nuget\nuget pack "deployd\deployd.csproj" -Properties Configuration=Release
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+.nuget\nuget pack "deployd-package\deployd-package.csproj" -Properties Configuration=Release
+if %errorlevel% neq 0 exit /b %errorlevel%

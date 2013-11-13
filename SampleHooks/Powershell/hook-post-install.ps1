e4d3bb54a3dd076de1514244ec1135be610f8526
andrewmyhre <andrew@justgiving.com>
2013-11-13 15:47:03 -0500

    adds sample powershell hooks

---
+++ SampleHooks/Powershell/hook-post-install.ps1
@@ -0,0 +1,45 @@
+﻿param([parameter(Mandatory=$false)][string]$Environment)
+
+# Set the name of your website/application
+$applicationName = "MyWebsite"
+
+Write-Host "This is a powershell script executed on the target server after $applicationName is installed"
+Write-Host "This installation is for the '$Environment' environment"
+Import-Module WebAdministration
+
+$website = Get-Website $applicationName
+
+if ($website)
+{
+	$bindings = Get-WebBinding $applicationName
+	Write-Host "$applicationName has $($bindings.length) bindings"
+	foreach($binding in $bindings)
+	{
+		$trimmed = $binding.bindingInformation.TrimEnd(":")
+		Write-Host "$applicationName has binding $trimmed"
+		Write-Host "This is the ip/port we would put in the pool"
+	}
+	try {
+		$appPool = Get-WebAppPool "$applicationName AppPool"
+		if ($appPool)
+		{
+			Start-WebAppPool "$applicationName AppPool"
+			Write-Host "Started application pool"
+		}
+	} catch
+	{
+
+	}
+
+	foreach($binding in $bindings)
+	{
+		Write-Host "Site warmup $trimmed"
+		$url = "http://"+$trimmed
+		$req=[system.Net.HttpWebRequest]::Create($url);
+		$res = $req.getresponse();
+		$stat = $res.statuscode;
+		$res.Close();
+	}
+
+	Write-Host "Site returned status code $stat"
+}
---
+++ SampleHooks/Powershell/hook-pre-install.ps1
@@ -0,0 +1,29 @@
+﻿param([parameter(Mandatory=$false)][string]$Environment)
+
+# Set the name of your website/application
+$applicationName = "MyWebsite"
+
+Write-Host "This is a powershell script executed on the target server before $applicationName is installed"
+Write-Host "This installation is for the '$Environment' environment"
+
+# Sample website administration
+Import-Module WebAdministration
+$website = Get-Website $applicationName
+
+if ($website)
+{
+	$bindings = Get-WebBinding $applicationName
+	Write-Host "$applicationName has $($bindings.length) bindings"
+	foreach($binding in $bindings)
+	{
+		$trimmed = $binding.bindingInformation.TrimEnd(":")
+		Write-Host "$applicationName has binding $trimmed"
+		Write-Host "This is the ip/port we would take out of the pool"
+	}
+
+	$appPool = Get-WebAppPool "$websiteName AppPool"
+	if ($appPool)
+	{
+		Stop-WebAppPool "$websiteName AppPool"
+	}
+}
